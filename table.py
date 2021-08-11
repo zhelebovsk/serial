70b243c5e2e2379d41a8d6d484388f3e2c28d28e
PC <qrabeq@gmail.com>
2021-08-11 15:58:43 +0300

    f

--- table.py
+++ table.py
@@ -8,13 +8,13 @@ def nothing(a):
     pass
 
 if __name__ == '__main__':
-    os.chdir('data')
-    vid = cv.VideoCapture(0)
-    m = 4
-    n = 6
+    #os.chdir('data')
+    #vid = cv.VideoCapture(0)
+    m = 21
+    n = 5
     num = 0
     pos = []
-    table_hc6 = serial.Serial('COM10')
+    table_hc6 = serial.Serial('COM12')
     for j in range(n):
         for i in range(m):
             if j % 2 == 0:
@@ -24,30 +24,32 @@ if __name__ == '__main__':
 
     current_pos = pos.pop(0)
     move = [0, 0]
-    ret, image = vid.read()
-    cv.imwrite(str(num) + '.' + str(current_pos) + '.bmp', image)
+    #ret, image = vid.read()
+    #cv.imwrite(str(num) + '.' + str(current_pos) + '.bmp', image)
     for i in pos:
         num += 1
         move[0] = i[0] - current_pos[0]
         move[1] = i[1] - current_pos[1]
         if move[0] != 0:
+            st_num = 'm'
             if move[0] == 1:
                 dir = 'b'
             else:
                 dir = 'f'
         else:
+            st_num = 'n'
             if move[1] == 1:
                 dir = 'r'
             else:
                 dir = 'l'
         table_hc6.write(dir.encode('utf-8'))
         time.sleep(0.05)
-        table_hc6.write('4'.encode('utf-8'))
-        time.sleep(1)
-        for s in range(5):
-            ret, image = vid.read()
-            cv.imwrite(str(num) + '.' + str(i) + str(s) + '.bmp', image)
-        time.sleep(1)
+        table_hc6.write(st_num.encode('utf-8'))
+        time.sleep(8)
+        #for s in range(5):
+        #    ret, image = vid.read()
+        #    cv.imwrite(str(num) + '.' + str(i) + str(s) + '.bmp', image)
+        #time.sleep(1)
         current_pos = i
 
 
